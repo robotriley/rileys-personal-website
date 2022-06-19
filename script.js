d9f07e21aa0d768138941b8096c9d5ffe306620a
riley soloner <rileysoloner@gmail.com>
2022-06-19 14:24:13 -0400

    add bio, update CSS, add photo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("H2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This content (meaning, this line and this line only) added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
